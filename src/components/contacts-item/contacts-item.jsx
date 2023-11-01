4c3419f964e1d04d324bffc4a05a5563e575a8a6
Jaba <jabaqadagishvili4@gmail.com>
2023-11-01 17:50:08 +0400

    fixed a bug on icons into ContactsItem component, now they appear when text is typed

--- src/components/contacts-item/contacts-item.jsx
+++ src/components/contacts-item/contacts-item.jsx
@@ -1,38 +1,34 @@
 import './contacts-item.css';
-import { Component } from 'react';
 import Icon from '@mdi/react';
 import { mdiEmail } from '@mdi/js';
 import { mdiPhoneMessage } from '@mdi/js';
 import { mdiMapMarker } from '@mdi/js';
-import { PersonalInformation } from '../personal-information/personal-Information';
 
-class ContactsItem extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { fullName, email, phoneNumber, address, setInformation } =
-      this.props;
-    return (
-      <div className='contacts-details'>
-        <div className='resume-name'>
-          <h1>{fullName}</h1>
-        </div>
-        <div className='resume-contacts'>
-          <h3 value={fullName}>
+function ContactsItem({ fullName, email, phoneNumber, address }) {
+  return (
+    <div className='contacts-details'>
+      <div className='resume-name'>
+        <h1>{fullName}</h1>
+      </div>
+      <div className='resume-contacts'>
+        {email.length > 0 && (
+          <h3>
             {<Icon path={mdiEmail} size={1} />} {email}
           </h3>
+        )}
+        {phoneNumber.length > 0 && (
           <h3>
             {<Icon path={mdiPhoneMessage} size={1} />} {phoneNumber}
           </h3>
+        )}
+        {address.length > 0 && (
           <h3>
             {<Icon path={mdiMapMarker} size={1} />} {address}
           </h3>
-        </div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export { ContactsItem };

e6a3d31fd700b1809290d7a8319a408f6f65f0a0
Jaba <jabaqadagishvili4@gmail.com>
2023-11-04 22:07:23 +0400

    fixed bug with page print

--- src/components/createdBy/createdBy.jsx
+++ src/components/createdBy/createdBy.jsx
@@ -5,11 +5,10 @@ function CreatedBy() {
     <div className='about-container'>
       <div className='about-me'>
         <h3>Created by</h3>
-        <button className='info-button'>
-          <a href='https://github.com/jabaaq' target='_blank'>
-            @jabaaq
-          </a>
-        </button>
+
+        <a href='https://github.com/jabaaq' target='_blank'>
+          <button className='info-button'>@jabaaq</button>
+        </a>
       </div>
     </div>
   );
